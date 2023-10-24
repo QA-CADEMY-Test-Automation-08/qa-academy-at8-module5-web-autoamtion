e8dc8126b74ec17a5796931308c9ec2d91de2f77
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2023-10-23 21:52:37 -0400

    Class1 - Core-UI-Automation

---
+++ core-ui-automation/src/main/java/com/qaacademy/module5/automation/core/ui/webdrivers/WebDriverTypes.java
@@ -0,0 +1,28 @@
+package com.qaacademy.module5.automation.core.ui.webdrivers;
+
+/**
+ * Driver types enum.
+ */
+public enum WebDriverTypes {
+    CHROME("CHROME");
+
+    private final String word;
+
+    /**
+     * Initializes WebDriver types enum.
+     *
+     * @param word original word.
+     */
+    WebDriverTypes(final String word) {
+        this.word = word;
+    }
+
+    /**
+     * Gets the enum word value.
+     *
+     * @return original word.
+     */
+    public String val() {
+        return word;
+    }
+}
